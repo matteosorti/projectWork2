ec1ec1f10a92f001b3101022f687c6fbc07a6f36
matteosorti <matteosorti99.ms@gmail.com>
2021-03-17 15:54:42 +0100

    first commit

---
+++ projectwork/src/main/java/it/jac/projectwork/ProjectworkApplication.java
@@ -0,0 +1,13 @@
+package it.jac.projectwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectworkApplication.class, args);
+	}
+
+}
