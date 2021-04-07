26da84fde3a0b6447a50c87a5ffeeea268885f32
Matteo Sorti <matteosorti@users.noreply.github.com>
2021-04-07 16:16:10 +0200

    response

---
+++ projectwork/src/main/java/it/jac/projectwork/dto/Response.java
@@ -0,0 +1,16 @@
+package it.jac.projectwork.dto;
+
+import lombok.Data;
+
+@Data
+public class Response<T> {
+	
+	private T result;
+	
+	private boolean resultTest;
+	
+	private String error;
+	
+	
+}
+
